5d0e6b8dbca6ec10bcd613c5da04ecf450a30203
bava-kurian <bavakurian135@gmail.com>
2025-01-26 20:28:24 +0530

    error

--- CarbonFootprint/main_app/forms.py
+++ CarbonFootprint/main_app/forms.py
@@ -4,19 +4,19 @@ from .models import EnergyConsumption, Transportation, WaterUsage, WasteManageme
 class EnergyConsumptionForm(forms.ModelForm):
     class Meta:
         model = EnergyConsumption
-        fields = '__all__'
+        exclude = ['yearly_data']
 
 class TransportationForm(forms.ModelForm):
     class Meta:
         model = Transportation
-        fields = '__all__'
+        exclude = ['yearly_data']
 
 class WaterUsageForm(forms.ModelForm):
     class Meta:
         model = WaterUsage
-        fields = '__all__'
+        exclude = ['yearly_data']
 
 class WasteManagementForm(forms.ModelForm):
     class Meta:
         model = WasteManagement
-        fields = '__all__'
+        exclude = ['yearly_data']
--- CarbonFootprint/main_app/models.py
+++ CarbonFootprint/main_app/models.py
@@ -9,7 +9,7 @@ class EmissionFactor(models.Model):
         return self.category
 
 class CollegeDetails(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE, default=1)  # Use the ID of the default user
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=100)
     city = models.CharField(max_length=100)
     total_campus_area = models.FloatField()
@@ -20,19 +20,24 @@ class CollegeDetails(models.Model):
     def __str__(self):
         return self.name
 
-class EnergyConsumption(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE,default=1)
+class YearlyData(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     year = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.user.username} - {self.year}"
+
+class EnergyConsumption(models.Model):
+    yearly_data = models.ForeignKey(YearlyData, on_delete=models.CASCADE)
     electricity_kwh = models.FloatField()
     solar_generation_kwh = models.FloatField()
     generator_fuel_liters = models.FloatField()
 
     def __str__(self):
-        return str(self.year)
+        return str(self.yearly_data.year)
 
 class Transportation(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE,default=1)
-    year = models.IntegerField()
+    yearly_data = models.ForeignKey(YearlyData, on_delete=models.CASCADE)
     total_students_staff = models.IntegerField()
     distance_by_bus_km = models.FloatField()
     distance_by_personal_vehicles_km = models.FloatField()
@@ -40,24 +45,22 @@ class Transportation(models.Model):
     distance_by_ev_km = models.FloatField()
 
     def __str__(self):
-        return str(self.year)
+        return str(self.yearly_data.year)
 
 class WaterUsage(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE,default=1)
-    year = models.IntegerField()
+    yearly_data = models.ForeignKey(YearlyData, on_delete=models.CASCADE)
     water_consumed_kl = models.FloatField()
     rainwater_harvesting_capacity = models.FloatField()
     wastewater_treated_kl = models.FloatField()
 
     def __str__(self):
-        return str(self.year)
+        return str(self.yearly_data.year)
 
 class WasteManagement(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE,default=1)
-    year = models.IntegerField()
+    yearly_data = models.ForeignKey(YearlyData, on_delete=models.CASCADE)
     waste_generated_tonnes = models.FloatField()
     waste_treated_tonnes = models.FloatField()
     waste_to_landfill_tonnes = models.FloatField()
 
     def __str__(self):
-        return str(self.year)
+        return str(self.yearly_data.year)
